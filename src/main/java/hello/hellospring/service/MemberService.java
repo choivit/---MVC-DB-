db1726e50caa51817ff9b714d7534183a1cefc0a
choivit <topvit5@hanmail.net>
2022-10-13 10:41:36 +0900

    221013 회원 서비스 개발

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,2 +1,48 @@
-package hello.hellospring.service;public class MemberService {
+package hello.hellospring.service;
+
+import hello.hellospring.Repository.MemberRepository;
+import hello.hellospring.Repository.MemoryMemberRepository;
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member){
+        //같은 이름이 있는 중복 회원X
+//        Optional<Member> result = memberRepository.findByName(member.getName()); //꺼낸 값이 null 일 가능성이 있으면 optional로 감싸서 빼주고 덕분에 하위가 가능함.
+//        result.ifPresent(m -> { //ifPresent == result가 값이 있으면(Optional이기 때문에 가능)
+//            throw new IllegalStateException("이미 존재하는 회원입니다.");
+//        });
+//        Member member1 = result.get(); //그냥 꺼내고 싶으면 이렇게 해도 상관은 없는데 권장하지 X
+//        result.orElseGet(); //값이 있으면 꺼내고 없으면 뭔가 실행하거나 기본값을 실행하라는 말.
+        validateDuplicateMember(member); //중복 회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
 }
--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,15 +1,25 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.Repository.MemberRepository;
+import hello.hellospring.Repository.MemoryMemberRepository;
 import hello.hellospring.domain.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class MemoryMemberRepositoryTest {
 
-    MemberRepository repository = new hello.hellospring.Repository.MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
 
     @Test
     public void save(){
@@ -22,7 +32,7 @@ public class MemoryMemberRepositoryTest {
 //        System.out.println("result  = " + (result == member));
 //        Assertions.assertEquals(member, result); //같을 경우 성공
 //        Assertions.assertEquals(member, null); //같지 않으면 이름 모를 error
-        Assertions.assertThat(member).isEqualTo(result);
+        assertThat(member).isEqualTo(result);
     }
 
     @Test
@@ -37,7 +47,22 @@ public class MemoryMemberRepositoryTest {
 
         Member result = repository.findByName("spring1").get();
 
-        Assertions.assertThat(result).isEqualTo(member1);
+        assertThat(result).isEqualTo(member1);
 
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member1.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
     }
 }
